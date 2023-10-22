ffa80fd837a3dd253e0ad97227851cd1feb5738f
Daniel Ces <plamster1@gmail.com>
2023-10-22 12:50:44 +0200

    added routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Blog from './Blog';
+import Home from './Home';
+import Photos from './Photos';
+import Profile from './Profile';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
